da148ad1af81fe78d0e8bed9e78d21c9286c5b20
jpablopachar <jpablopachar1993@gmail.com>
2024-02-22 02:57:59 -0500

    Creación y configuración de ambientes

---
+++ client/src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  name: 'production',
+  actionCodeSettings: {
+    url: 'https://ecommerce-e4e6c.web.app/profile/new',
+    handleCodeInApp: true
+  },
+  url: 'http://localhost:4200'
+};
---
+++ client/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  name: 'default',
+  actionCodeSettings: {
+    url: 'http://localhost:4200/profile/new',
+    handleCodeInApp: true
+  },
+  url: 'http://localhost:4200'
+};
