d2605a60fe191dab2f3124895c07a3a9fac4dca2
jpablopachar <jpablopachar1993@gmail.com>
2024-02-26 01:40:59 -0500

    Se agrega configuraciones de seguridad a la aplicación

--- server/src/WebApi/Program.cs
+++ server/src/WebApi/Program.cs
@@ -1,8 +1,13 @@
+using System.Text;
 using AutoMapper;
 using BusinessLogic.Data;
 using BusinessLogic.Logic;
+using Core.Entities;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WebApi.Dtos;
 using WebApi.Middlewares;
 
@@ -10,11 +15,36 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<MarketDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnection")));
 
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthConnection")));
+
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericAuthRepository<>), typeof(GenericAuthRepository<>));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+var builderIdentity = builder.Services.AddIdentityCore<User>();
+
+builderIdentity = new IdentityBuilder(builderIdentity.UserType, builderIdentity.Services);
+
+builderIdentity.AddRoles<IdentityRole>();
+
+builderIdentity.AddEntityFrameworkStores<AuthDbContext>();
+builderIdentity.AddSignInManager<SignInManager<User>>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:Key"]!)),
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+        ValidateIssuer = true,
+        ValidateAudience = false
+    };
+});
 
 var mapperConfig = new MapperConfiguration(mc => mc.AddProfile(new MappingProfiles()));
 
@@ -53,6 +83,10 @@ app.UseStatusCodePagesWithReExecute("/errors", "?code={0}");
 
 app.UseCors("CorsRule");
 
+app.UseAuthentication();
+
+app.UseAuthorization();
+
 app.MapControllers();
 
 using (var environment = app.Services.CreateScope())
@@ -63,17 +97,17 @@ using (var environment = app.Services.CreateScope())
     try
     {
         var context = services.GetRequiredService<MarketDbContext>();
+        var userManager = services.GetRequiredService<UserManager<User>>();
+        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+        var identityContext = services.GetRequiredService<AuthDbContext>();
 
         await context.Database.MigrateAsync();
 
         await MarketDbContextData.LoadDataAsync(context, loggerFactory);
 
-        /* var userManager = services.GetRequiredService<UserManager<User>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-        var identityContext = services.GetRequiredService<AuthDbContext>(); */
+        await identityContext.Database.MigrateAsync();
 
-        // await identityContext.Database.MigrateAsync();
-        // await AuthDbContextData.SeedUserAsync(userManager, roleManager);
+        await AuthDbContextData.SeedUserAsync(userManager, roleManager);
     }
     catch (Exception exception)
     {
