faa021c829e131b1134b91d03a3a294b92a6115f
jpablopachar <jpablopachar1993@gmail.com>
2024-02-26 01:42:52 -0500

    Se agrega configuración al mapeo De Address y User

--- server/src/WebApi/Dtos/MappingProfile.cs
+++ server/src/WebApi/Dtos/MappingProfile.cs
@@ -11,15 +11,9 @@ namespace WebApi.Dtos
                 .ForMember(product => product.CategoryName, options => options.MapFrom(product => product.Category!.Name))
                 .ForMember(product => product.BrandName, options => options.MapFrom(product => product.Brand!.Name));
 
-            /* CreateMap<Core.Entities.Address, AddressDto>().ReverseMap();
+            CreateMap<Address, AddressDto>().ReverseMap();
 
             CreateMap<User, UserDto>().ReverseMap();
-
-            CreateMap<AddressDto, Core.Entities.PurchaseOrder.Address>().ReverseMap();
-
-            CreateMap<PurchaseOrders, PurchaseOrderResponseDto>().ForMember(o => o.ShippingType, x => x.MapFrom(y => y.ShippingType.Name)).ForMember(o => o.ShippingTypePrice, x => x.MapFrom(y => y.ShippingType.Price));
-
-            CreateMap<OrderItem, OrderItemResponseDto>().ForMember(o => o.ProductId, x => x.MapFrom(y => y.ItemOrdered.ProductItemId)).ForMember(o => o.ProductName, x => x.MapFrom(y => y.ItemOrdered.ProductName)).ForMember(o => o.ProductImage, x => x.MapFrom(y => y.ItemOrdered.ImageUrl)); */
         }
     }
 }
