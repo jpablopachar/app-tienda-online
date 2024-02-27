368ddc33762409e6138dbee719204449753025cd
jpablopachar <jpablopachar1993@gmail.com>
2024-02-27 00:47:33 -0500

    Se instala ngrx

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core'
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  isDevMode,
+} from '@angular/core'
 import {
   FirebaseApp,
   initializeApp,
@@ -12,18 +16,51 @@ import {
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideRouter } from '@angular/router'
 
+import { HttpClientModule } from '@angular/common/http'
+import {
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  MatDateFormats,
+} from '@angular/material/core'
+import { provideStoreDevtools } from '@ngrx/store-devtools'
 import { environment } from '@src/environments/environment'
 import { routes } from './app.routes'
 
+const APP_DATE_FORMATS: MatDateFormats = {
+  parse: {
+    dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' },
+  },
+  display: {
+    dateInput: { day: 'numeric', month: 'short', year: 'numeric' },
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  },
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    HttpClientModule,
     provideRouter(routes),
     provideAnimations(),
+    // provideHttpClient(withInterceptors([authInterceptor])),
     importProvidersFrom([
       provideFirebaseApp(
         (): FirebaseApp => initializeApp(environment.firebaseConfig)
       ),
       provideStorage((): FirebaseStorage => getStorage()),
     ]),
+    // provideStore(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+      autoPause: true,
+      trace: false,
+      traceLimit: 75,
+    }),
+    // provideEffects(),
+    // provideState('user', userReducers),
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS },
   ],
 };
